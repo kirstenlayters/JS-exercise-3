065b0002a59a1cf0464e52b9cf19dccc525040a9
kirstenlayters <kirsten.layters@gmail.com>
2026-05-22 10:10:18 +0200

    Delete arrays.js

--- arrays.js
+++
@@ -1,7 +0,0 @@
-let colors = []
-colors.push("green", "purple", "blue");
-let numbers = ["5", "10", "15", "20", "25"]
-colors.splice(2,1);
-colors.unshift("yellow");
-console.log("Length of the color array:" + " " + colors + ". ")
-console.log("Length of numbers array:" + " " + numbers + ". ")
